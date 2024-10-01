e522868357b2ed6e046e242af88b34ea52ba603e
Jared Ramon Elizan <elizanjaredr@gmail.com>
2024-10-01 09:13:06 +0800

    Add files via upload

---
+++ src/PremiumCalculator.java
@@ -0,0 +1,16 @@
+public class PremiumCalculator {
+    int calculatePremium(int premiumAmount, int accidentHistory, int drivingExperience){
+        premiumAmount += (7500 * accidentHistory);
+        if(drivingExperience < 3){
+            premiumAmount += 3000;
+        } else if(drivingExperience > 5){
+            premiumAmount -= (premiumAmount * .10);
+        } else if(drivingExperience > 10){
+            premiumAmount -= 1500;
+            premiumAmount -= (premiumAmount * .20);
+        }
+
+        return premiumAmount;
+    } 
+    
+}
